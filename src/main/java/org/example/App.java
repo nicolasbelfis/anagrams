7e1987657ff6e8504ecf2af7e0e2248a97a29c14
nicolasbelfis <nbelfis@gmail.com>
2023-08-06 14:14:40 +0100

    refactor , extract method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,11 @@ public class App {
 	public static boolean areAnagrams(String sentence1, String sentence2) {
 		if(sentence1 == null || sentence2 == null) return false;
 		if(sentence1.equals("") || sentence2.equals("")) return false;
-		return sentence1.replaceAll("[^a-zA-Z]", "")
-				.equalsIgnoreCase(sentence2.replaceAll("[^a-zA-Z]", ""));
+		return removePunctuation(sentence1)
+				.equalsIgnoreCase(removePunctuation(sentence2));
+	}
+
+	private static String removePunctuation(String sentence1) {
+		return sentence1.replaceAll("[^a-zA-Z]", "");
 	}
 }
