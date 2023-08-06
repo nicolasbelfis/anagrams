56d663228ede8258d205842c7f4093c231e023b6
nicolasbelfis <nbelfis@gmail.com>
2023-08-06 15:22:09 +0100

    refactor, method usage and extrat

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,32 +8,41 @@ import java.util.stream.Stream;
 public class App {
 
   public static boolean areAnagrams(String sentence1, String sentence2) {
-    if (sentence1 == null || sentence2 == null) {
+    if (isNull(sentence1) || isNull(sentence2)) {
       return false;
     }
-    if (sentence1.equals("") || sentence2.equals("")) {
+    if (sentence1.isEmpty() || sentence2.isEmpty()) {
       return false;
     }
 
-    Stream<Character> sentence1Stream = sentence1.chars()
-        .filter(Character::isAlphabetic)
-        .map(Character::toLowerCase)
-        .mapToObj(operand -> (char) operand);
+    Stream<Character> sentence1Stream = getLettersLowerCaseStream(sentence1);
 
-    Stream<Character> sentence2Stream = sentence2.chars()
-        .filter(Character::isAlphabetic)
-        .map(Character::toLowerCase)
-        .mapToObj(operand -> (char) operand);
+    Stream<Character> sentence2Stream = getLettersLowerCaseStream(sentence2);
 
 
     Map<Character, Long> sentence1CharCount =
         sentence1Stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-    boolean charAllMatchedAtLeastOnce = sentence2Stream
-        .peek(charSentence2 -> sentence1CharCount.computeIfPresent(charSentence2, (unused, aLong) -> --aLong))
+    boolean charsAllMatchedAtLeastOnce = sentence2Stream
+        .peek(charSentence2 -> decreaseCountOfCharInMap(sentence1CharCount, charSentence2))
         .allMatch(charSentence2 -> sentence1CharCount.get(charSentence2) != null);
 
-    return sentence1CharCount.values().stream().allMatch(aLong -> aLong == 0 && charAllMatchedAtLeastOnce);
+    return sentence1CharCount.values().stream().allMatch(aLong -> aLong == 0 && charsAllMatchedAtLeastOnce);
+  }
+
+  private static Long decreaseCountOfCharInMap(Map<Character, Long> sentence1CharCount, Character charSentence2) {
+    return sentence1CharCount.computeIfPresent(charSentence2, (unused, aLong) -> --aLong);
+  }
+
+  private static Stream<Character> getLettersLowerCaseStream(String sentence1) {
+    return sentence1.chars()
+        .filter(Character::isAlphabetic)
+        .map(Character::toLowerCase)
+        .mapToObj(operand -> (char) operand);
+  }
+
+  private static boolean isNull(String sentence1) {
+    return sentence1 == null;
   }
 
 }
